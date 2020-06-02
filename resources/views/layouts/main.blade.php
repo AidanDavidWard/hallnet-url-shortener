57280d9eed882461e179caa2cd7cc23315740bb7
Aidan David Ward <aidanward@airiq.co.uk>
2020-06-02 11:59:29 +0100

    Added virtualhost example

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>HB-BIT @yield('title')</title>
+        <title>HN-BIT @yield('title')</title>
 
         <link rel="stylesheet" href="/css/app.css">
 
